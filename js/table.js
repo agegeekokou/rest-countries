ae3d12f7f52919de72633fd1f74ef0247b82dd16
Kokou Agegee <delaliage@yahoo.fr>
2022-01-13 18:16:30 +0100

    second commit part 2

--- js/table.js
+++ js/table.js
@@ -2,15 +2,13 @@ async function render(){
     const response = await fetch("https://restcountries.com/v3.1/region/europe?fields=name,area,population,capital");
     const countries = await response.json();
     let tableOfCountries = "";
-    let formatNumber = new Intl.NumberFormat('en-EN')
     for(let country of countries){
-        country.area = formatNumber.format(country.area);
-        country.population = formatNumber.format(country.population);
+        
         tableOfCountries += `
         <tr>
             <th class="text-nowrap">${country.name.official}</th> 
-            <td>${country.area}</td> 
-            <td>${country.population}</td> 
+            <td>${new Intl.NumberFormat('en-EN').format(country.area)}</td> 
+            <td>${new Intl.NumberFormat('en-EN').format(country.population)}</td> 
             <td class="text-nowrap">${country.capital}</td>
         </tr>`;
     }
