3abea7fd69a642d19adf4b743017fd669e9d88fb
Kokou Agegee <delaliage@yahoo.fr>
2022-01-28 11:13:55 +0100

    part 4

---
+++ js/geolocation.js
@@ -0,0 +1,39 @@
+
+async function render(){
+    let response = await fetch("https://restcountries.com/v3.1/region/europe?fields=name,maps,cca2,capital");
+    const countries = await response.json();
+    let listOfCountries = "";
+    for(let country of countries){
+        if(country.cca2 == "FR"){
+            listOfCountries += `<option value="${country.cca2}" selected>${country.name.official}</option>`;
+        }else{
+            listOfCountries += `<option value="${country.cca2}">${country.name.official}</option>`;
+        
+        }
+    }
+    const element = document.querySelector("#countries");
+    element.innerHTML = listOfCountries;
+    
+// Affichage de la carte de la France    
+    response = await fetch(`https://restcountries.com/v3.1/alpha/FR?fields=capitalInfo`);
+    const france = await response.json();
+    let mapsOfCountries = `https://www.google.com/maps/embed/v1/view?key=AIzaSyAWHSmK2mrqS2Dg_E8DPygukjvKPCOqcKk&center=${france.capitalInfo.latlng[0]},${france.capitalInfo.latlng[1]}&zoom=10&language=en`;
+    let map = document.querySelector("#maps");
+    map.src = mapsOfCountries;
+     
+}
+// Affichage de la carte des autres pays
+async function changeMap(event){
+    const responseMaps = await fetch(`https://restcountries.com/v3.1/alpha/${event.target.value}?fields=capitalInfo`);
+    const country = await responseMaps.json();
+    let mapsOfCountries = `https://www.google.com/maps/embed/v1/view?key=AIzaSyAWHSmK2mrqS2Dg_E8DPygukjvKPCOqcKk&center=${country.capitalInfo.latlng[0]},${country.capitalInfo.latlng[1]}&zoom=10&language=en`;
+    let map = document.querySelector("#maps");
+    map.src = mapsOfCountries;    
+}
+// Gestion des evenements
+window.addEventListener("load", render());
+
+let countries = document.querySelector("#countries");
+countries.addEventListener("change", function(event){
+    changeMap(event);
+});
